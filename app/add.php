7b986ef6ed99196271a854e647026a80947524ff
Nirmal Tamang <nirmaltmg001@gmail.com>
2024-01-31 16:10:45 +0545

    initial commit

---
+++ app/add.php
@@ -0,0 +1,24 @@
+<?php
+
+if(isset($_POST['title'])){
+    require'../db_conn.php';
+
+    $title =$_POST['title'];
+
+    if(empty($title)){
+        header("location: ../index.php?mess=error");
+    }else{
+        $stmt = $conn->prepare("INSERT INTO todos(title) VALUE(?)");
+        $res =  $stmt->execute([$title]);
+
+        if($res) {
+            header("location: ../index.php?mess=success");
+        }else {
+            header("location: ../index.php");
+        }
+        $conn = null;
+        exit();
+    }
+}else {
+    header("location: ../index.php?mess=error");
+}
---
+++ index.php
@@ -0,0 +1,69 @@
+<?php
+require 'db_conn.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TODOlist</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="main-section">
+        <div class="add-section">
+             <form action="app/add.php" method="POST" autocomplete="off">
+                <?php if(isset($_GET['mess']) && $_GET['mess'] == 'error'){?>
+                     <input type="text" 
+                          name="title"
+                          style="border-color: red"
+                         placeholder="This field is required" />
+                    <button type="submit">ADD &nbsp; <span>&#43;</span> </button>
+
+                  <?php }else{ ?>
+                    <input type="text" 
+                           name="title"
+                          placeholder="what do you need to do?" />
+                    <button type="submit">ADD &nbsp; <span>&#43;</span> </button>
+                 <?php }?>
+             </form>
+          </div>
+          <?php 
+               $todos = $conn->query("SELECT * FROM todos ORDER BY id DESC");
+             ?>
+          
+              <div class="show-todo-section">
+                 <?php if($todos->rowCount() <= 0) {?>
+                      <div class="todo-item">
+                             <div class="empty">     
+                                 <img src="img/f.png" width="100%"/>
+                                 <img src="img/Ellipsis (1).gif" width="90px">
+                           </div>
+                       </div>
+                    <?php }?>
+
+                    <?php while($todo = $todos->fetch(PDO::FETCH_ASSOC)) { ?>
+                 <div class="todo-item">
+                            <span id="<?php echo $todo['id']; ?>"
+                            class="remove-to-do">x</span>
+                            <?php if ($todo['checked']) {?>
+                                <input type="checkbox"
+                                     class="check-box"
+                                     checked />
+                                <h2 class="checked"><?php echo $todo['title']?></h2>
+                            <?php } else{ ?>
+                                 <input type="checkbox"
+                                 class="check-box" />
+                                 <h2><?php echo $todo['title']?></h2>  
+                            <?php }?>
+                            <br>
+                            <small>created:<?php echo $todo['data_time']?></small>
+                 </div>
+                 <?php }?>
+            </div>
+    </div>
+</body>
+
+</html>
